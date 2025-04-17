a348f766de76822afbd80d9ac226d6524e1e614e
mvsejin <mvsejin@gmail.com>
2025-04-17 14:55:57 +0900

    Update MongoDbJob.java

--- Chapter07/MongoDbJob.java
+++ Chapter07/MongoDbJob.java
@@ -18,35 +18,6 @@ public class MongoDbJob {
 //	@Autowired
 //	private StepBuilderFactory stepBuilderFactory;
 //
-//	@Bean
-//	@StepScope
-//	public MongoItemReader<Map> tweetsItemReader(MongoOperations mongoTemplate,
-//			@Value("#{jobParameters['hashTag']}") String hashtag) {
-//		return new MongoItemReaderBuilder<Map>()
-//				.name("tweetsItemReader")
-//				.targetType(Map.class)
-//				.jsonQuery("{ \"entities.hashtags.text\": { $eq: ?0 }}")
-//				.collection("tweets_collection")
-//				.parameterValues(Collections.singletonList(hashtag))
-//				.pageSize(10)
-//				.sorts(Collections.singletonMap("created_at", Sort.Direction.ASC))
-//				.template(mongoTemplate)
-//				.build();
-//	}
-//
-//	@Bean
-//	public ItemWriter<Map> itemWriter() {
-//		return (items) -> items.forEach(System.out::println);
-//	}
-//
-//	@Bean
-//	public Step copyFileStep() {
-//		return this.stepBuilderFactory.get("copyFileStep")
-//				.<Map, Map>chunk(10)
-//				.reader(tweetsItemReader(null, null))
-//				.writer(itemWriter())
-//				.build();
-//	}
 //
 //	@Bean
 //	public Job job() {
